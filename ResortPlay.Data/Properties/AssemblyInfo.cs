54dd577b7877ad6792f3c725f2630e660f631737
Simeon Kovachev <skcodingacc@gmail.com>
2023-01-07 18:30:34 +0200

    Installed EntityFramework; Added the DBContext class and configured it and enabled migrations.

--- ResortPlay.Data/Properties/AssemblyInfo.cs
+++ ResortPlay.Data/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("48769845-8f34-4b53-a6bb-b1bae6595c41")]
+[assembly: Guid("0fe495f0-6b59-451e-b916-5a50da09165a")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ ResortPlay.Data/ResortPlayContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using ResortPlay.Entity;
+
+namespace ResortPlay.Data
+{
+    public class ResortPlayContext : DbContext
+    {
+        public ResortPlayContext() : base("HMSConnectionString")
+        {
+        }
+
+        public DbSet<AccomodationType> AccomodationTypes { get; set; }
+        public DbSet<AccomodationPackage> AccomodationPackages { get; set; }
+        public DbSet<Accomodation> Accomodations { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
