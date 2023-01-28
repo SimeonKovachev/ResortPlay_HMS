eecaedf71b9f86eaae0142f2da8b72bc1f6b7328
Simeon Kovachev <skcodingacc@gmail.com>
2023-01-28 12:07:42 +0200

    Added a carousal in the home page. A lot of nerves :)

---
+++ ResortPlay/ViewModels/HomeViewModels.cs
@@ -0,0 +1,13 @@
+﻿using ResortPlay.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ResortPlay.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<AccomodationType> AccomodationTypes { get; set; }
+    }
+}
